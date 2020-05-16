d39fca0ec1f801dc6596f117ac648b5d9ab48d7a
Andrej Nikolić <existence.of.utopia@gmail.com>
2020-05-16 14:13:46 +0200

    Header

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+    <title>Ticket Reservator - Welcome</title>
+</head>
+<body>
+    <?php include "header.php" ?>
+    <h1>
+        test
+        <span class="test-class">Test file</span>
+    </h1>
+</body>
+</html>
