dc764ce8f219a482936f08bbfa4cd2f57faf6469
Andrej Nikolić <existence.of.utopia@gmail.com>
2020-05-24 15:09:32 +0200

    Homepage part1

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 </head>
 <body>
 <?php include "assets/header.php" ?>
-    <div class="container my-5">
+    <div class="container py-4">
         <h1>Homepage</h1>
         <p>Here you need to add slider and full description and options of Ticket Reservator</p>
         <ul>
@@ -34,6 +34,30 @@
             <li>Thank you page with order details</li>
             <li>Responsive</li>
         </ul>
+        <div class="row">
+            <div class="col-md-5 align-self-center">
+                <h3>Login and Register</h3>
+                <p>This website allows user to create his own account via registration form to order tickets. He can latter on log in and use that account to order new tickets.</p>
+                <p>User can't order any tickets without loging in first.</p>
+            </div>
+            <div class="col-md-7">
+                <img class="img-fluid home-image" src="img/login-register.jpg" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="conatainer-fluid bg-light py-4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7">
+                    <img class="img-fluid" src="img/admin-01.png" alt="">
+                </div>
+                <div class="col-md-5 align-self-center">
+                    <h3>Administrator</h3>
+                    <p>There is a huge difference between user and administrator, which we will talk more about in the text bellow. 
+                    <p>For now, we will just mention that there is administrator that allows you to use "Admin Panel" and "Add Concert" (for adding new concert, of course). Administrator can also <strong>delete</strong> and <strong>edit</strong> created concerts.</p>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
